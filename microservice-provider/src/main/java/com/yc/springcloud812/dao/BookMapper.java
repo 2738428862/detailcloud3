8ae1131c7c9263137fd898c6dddcf5a04d30ccb5
2738428862 <2738428862@qq.com>
2020-10-11 16:44:32 +0800

    Revert "项目二"
    This reverts commit 738919fe

--- microservice-provider/src/main/java/com/yc/springcloud812/dao/BookMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yc.springcloud812.dao;
-
-import com.yc.springcloud812.domain.Book;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BookMapper extends MisBaseMapper<Book> {
-}
